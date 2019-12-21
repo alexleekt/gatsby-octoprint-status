30cb21221b14461cb0761a49c4525029115ec71a
Alex Lee <stratrix@gmail.com>
2019-12-20 22:48:14 -0800

    prettier: formatting changes by prettier.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react"
 import Moment from "react-moment"
-import './styles.css'
-import Axios from "axios";
+import "./styles.css"
+import Axios from "axios"
 import * as parse from "url-parse"
 
 // local storage keys
-const lsk_config = "lsk_config";
+const lsk_config = "lsk_config"
 
 const OctoprintStatus = props => {
   const [config, setConfig] = useState(
@@ -16,56 +16,56 @@ const OctoprintStatus = props => {
     }
   )
   useEffect(() => {
-    localStorage.setItem(lsk_config, JSON.stringify(config));
-    requestUpdate();
+    localStorage.setItem(lsk_config, JSON.stringify(config))
+    requestUpdate()
     setInterval(() => {
-      requestUpdate();
-    }, 60*1000);
-  }, [config]);
+      requestUpdate()
+    }, 60 * 1000)
+  }, [config])
 
   const [state, setState] = useState({
     lastUpdated: 0,
     printer: {},
     job: {},
     settings: {},
-  });
+  })
 
-  const validateConfig = async (event) => {
+  const validateConfig = async event => {
     if (config.validated) {
-      console.log("already validated. ignoring this request.");
-      return true;
+      console.log("already validated. ignoring this request.")
+      return true
     }
     try {
-      const response = await reqGet("/api/settings");
+      const response = await reqGet("/api/settings")
       setConfig({
         ...config,
         validated: true,
       })
-      return true;
+      return true
     } catch (error) {
       setConfig({
         ...config,
         validated: false,
       })
-      return false;
+      return false
     }
   }
 
   const requestUpdate = async () => {
     if (!config.validated) {
-      console.error("invalid config. skipping requestUpdate().");
-      return;
+      console.error("invalid config. skipping requestUpdate().")
+      return
     }
-    const [s, p, j] = await Promise.all([reqGet('/api/settings'), reqGet('/api/printer'), reqGet('/api/job')])
+    const [s, p, j] = await Promise.all([reqGet("/api/settings"), reqGet("/api/printer"), reqGet("/api/job")])
     setState({
       ...state,
       settings: s,
       printer: p,
       job: j,
-      lastUpdated: Date.now()
+      lastUpdated: Date.now(),
     })
 
-    console.dir(state);
+    console.dir(state)
   }
 
   const reqGet = async path => {
@@ -105,7 +105,7 @@ const OctoprintStatus = props => {
 
   const snapshotUrl = () => {
     try {
-      const url = parse(config.server);
+      const url = parse(config.server)
       return `${state.settings.webcam.snapshotUrl.replace("127.0.0.1", url.host)}&timestamp=${new Date().getTime()}`
     } catch (error) {
       return "https://cataas.com/cat"
@@ -117,47 +117,51 @@ const OctoprintStatus = props => {
       <div>
         <div>
           <input
-            onChange={e => setConfig({
-              ...config,
-              validated: false,
-              server: e.target.value,
-              })}
+            onChange={e =>
+              setConfig({
+                ...config,
+                validated: false,
+                server: e.target.value,
+              })
+            }
             placeholder="e.g. http://octopi.local"
             defaultValue={config.server}
             onKeyPress={event => {
-              if (event.key === 'Enter') {
-                validateConfig(event);
+              if (event.key === "Enter") {
+                validateConfig(event)
               }
             }}
-            />
+          />
         </div>
         <div>
           <input
-            onChange={e => setConfig({
-              ...config,
-              validated: false,
-              apiKey: e.target.value,
-              })}
+            onChange={e =>
+              setConfig({
+                ...config,
+                validated: false,
+                apiKey: e.target.value,
+              })
+            }
             placeholder="Copy from Octopi (enable CORS)"
             defaultValue={config.apiKey}
             onKeyPress={event => {
-              if (event.key === 'Enter') {
-                validateConfig(event);
+              if (event.key === "Enter") {
+                validateConfig(event)
               }
             }}
-            />
+          />
         </div>
         <div>
           <input
             type="button"
             value="Save"
             onKeyPress={event => {
-              if (event.key === 'Enter') {
-                validateConfig(event);
+              if (event.key === "Enter") {
+                validateConfig(event)
               }
             }}
             onClick={validateConfig}
-            />
+          />
         </div>
       </div>
       <div onClick={requestUpdate}>
@@ -166,7 +170,10 @@ const OctoprintStatus = props => {
         ) : (
           <>
             <div>
-              {state.printer.state.text} - {Math.round(state.job.progress.completion)}% finishing <Moment add={{ seconds: state.job.progress.printTimeLeft}} fromNow>{Date()}</Moment>
+              {state.printer.state.text} - {Math.round(state.job.progress.completion)}% finishing{" "}
+              <Moment add={{ seconds: state.job.progress.printTimeLeft }} fromNow>
+                {Date()}
+              </Moment>
             </div>
             <div>{state.job.job.file.display}</div>
             <div>
@@ -176,10 +183,7 @@ const OctoprintStatus = props => {
               bed {state.printer.temperature.bed.actual} -> {state.printer.temperature.bed.target}
             </div>
             <div>
-            <img
-              src={snapshotUrl()}
-              className="img-hor-vert"
-              />
+              <img src={snapshotUrl()} className="img-hor-vert" />
             </div>
           </>
         )}
